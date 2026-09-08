bb0784197fdf747c0fa90f50719ec2395a86a6e9
gkb7 <138383288+gkb7@users.noreply.github.com>
2026-09-08 19:49:07 +0530

    Update conv_tile.cpp

--- task1/src/conv_tile.cpp
+++ task1/src/conv_tile.cpp
@@ -9,35 +9,31 @@ void conv_tile(const float* in, float* out, const float* ker,
     // conv_naive(in, out, ker, H, W, K);
 
     constexpr int TILE = 128;
-
-    const int p = K / 2;
-    const int in_stride = W + 2 * p;
-
-    // processing output tile by tile
-    for (int ii = 0; ii < H; ii += TILE) {
-        for (int jj = 0; jj < W; jj += TILE) {
+    const int p = K/2;
+    const int in_stride = W + 2*p;
+  
+    //processing output tile by tile
+    for (int ii = 0; ii < H; ii+=TILE) {
+        for (int jj = 0; jj < W; jj +=TILE) {
 
             const int i_end = std::min(ii + TILE, H);
             const int j_end = std::min(jj + TILE, W);
-
     // initializing curr output tile.
             for (int i = ii; i < i_end; i++) {
-                for (int j = jj; j < j_end; j++) {
+                for (int j = jj; j < j_end;j++) {
                     out[i * W + j] = 0.0f;
                 }
             }
 
     // apply every kernel element to the current tile.
-            for (int ki = 0; ki < K; ki++) {
+            for (int ki = 0;ki < K; ki++) {
                 for (int kj = 0; kj < K; kj++) {
 
-                    const float weight = ker[ki * K + kj];
-                    for (int i = ii; i < i_end; i++) {
-                        for (int j = jj; j < j_end; j++) {
+                    const float w = ker[ki * K + kj];
+                    for(int i =ii; i< i_end;i++) {
+                        for(int j =jj; j< j_end;j++) {
 
-                            out[i * W + j] +=
-                                in[(i + ki) * in_stride + (j + kj)]
-                                * weight;
+                            out[i * W + j] += in[(i + ki) * in_stride + (j + kj)]* w;
                         }
                     }
                 }
